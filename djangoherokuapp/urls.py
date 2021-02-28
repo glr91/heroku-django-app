8277a09364fbb1d4adc5d374e5cf2679781abf50
Guille <glr913@gmail.com>
2021-02-28 14:20:31 +0100

    Add main and API urls to backend

--- djangoherokuapp/urls.py
+++ djangoherokuapp/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from rest_framework import routers                     
+from django.views.generic import TemplateView
+from cds_guille import views 
+
+router = routers.DefaultRouter()   
+router.register(r'albums', views.AlbumsView, 'album')
+router.register(r'bands', views.BandsView, 'band')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
