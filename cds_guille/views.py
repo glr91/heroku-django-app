53e3eaa398ffa73915c5d7ee72adf2d1627cee4d
Guille <glr913@gmail.com>
2021-02-28 21:16:01 +0100

    Fix main view

--- cds_guille/views.py
+++ cds_guille/views.py
@@ -3,8 +3,10 @@ from rest_framework import viewsets
 from .serializers import AlbumSerializer, BandSerializer     
 from .models import Album, Band    
 from django.views import View
-from django.http import HttpResponse, HttpResponseNotFound
-import os                
+from django.http import HttpResponse
+from django.conf import settings
+import os     
+import logging      
 
 class AlbumsView(viewsets.ModelViewSet):       
     serializer_class = AlbumSerializer          
@@ -14,12 +16,24 @@ class BandsView(viewsets.ModelViewSet):
     serializer_class = BandSerializer          
     queryset = Band.objects.all()       
 
-class Assets(View):
-    def get(self, _request, filename):
-        path = os.path.join(os.path.dirname(__file__), 'static', filename)
+class FrontendAppView(View):
+    """
+    Serves the compiled frontend entry point (only works if you have run `yarn
+    build`).
+    """
+    index_file_path = os.path.join(settings.BASE_DIR, 'build', 'index.html')
 
-        if os.path.isfile(path):
-            with open(path, 'rb') as file:
-                return HttpResponse(file.read(), content_type='application/javascript')
-        else:
-            return HttpResponseNotFound()
+    def get(self, request):
+        try:
+            with open(self.index_file_path) as f:
+                return HttpResponse(f.read())
+        except FileNotFoundError:
+            logging.exception('Production build of app not found')
+            return HttpResponse(
+                """
+                This URL is only used when you have built the production
+                version of the app. Visit http://localhost:3000/ instead after
+                running `yarn start` on the frontend/ directory
+                """,
+                status=501,
+            )
