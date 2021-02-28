3e6afbaba9449a46282d1b4531dc8b6ff31ad296
Guille <glr913@gmail.com>
2021-02-28 13:51:08 +0100

    Add some simple models and views

--- cds_guille/models.py
+++ cds_guille/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+# -*- coding: utf-8 -*-
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=500)
+    year = models.IntegerField()
+    artist = models.ForeignKey('Band', on_delete=models.CASCADE)
+    original = models.BooleanField()
+
+    class Meta:
+        ordering = ('year',)
+
+class Band(models.Model):
+    name = models.CharField(max_length=500)
--- cds_guille/views.py
+++ cds_guille/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets          
+from .serializers import AlbumSerializer, BandSerializer     
+from .models import Album, Band                    
 
-# Create your views here.
+class AlbumsView(viewsets.ModelViewSet):       
+    serializer_class = AlbumSerializer          
+    queryset = Album.objects.all()          
+
+class BandsView(viewsets.ModelViewSet):       
+    serializer_class = BandSerializer          
+    queryset = Band.objects.all()       
