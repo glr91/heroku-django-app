50111a803e2f0a2fb9367112594e03ef3768fa40
Guille <glr913@gmail.com>
2021-02-28 21:15:43 +0100

    Fix Django settings

--- djangoherokuapp/settings.py
+++ djangoherokuapp/settings.py
@@ -134,13 +134,13 @@ STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-     os.path.join(BASE_DIR, 'build/static'),
+     os.path.join(BASE_DIR, 'build', 'static'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
