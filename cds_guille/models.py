f313fae5f4841a1f41b66d0d2e2b14487778c2e2
Guille <glr913@gmail.com>
2021-03-10 20:19:04 +0100

    Make original/year fields of Album model nullable

--- cds_guille/models.py
+++ cds_guille/models.py
@@ -6,9 +6,9 @@ from django.conf import settings
 
 class Album(models.Model):
     title = models.CharField(max_length=500)
-    year = models.IntegerField()
+    year = models.IntegerField(blank=True, null=True)
     artist = models.ForeignKey('Band', on_delete=models.CASCADE)
-    original = models.BooleanField()
+    original = models.BooleanField(blank=True, null=True)
 
     class Meta:
         ordering = ('year',)
